5e014d14e8ed366eb7f5e0c6d3ac4a495d470f09
serhatprogramming <serhatprogramming@gmail.com>
2023-03-24 17:29:35 -0400

    user creation error validation unit test

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post("/", async (request, response, next) => {
   const { username, name, password } = request.body;
 
   const saltRounds = 10;
@@ -13,10 +13,18 @@ usersRouter.post("/", async (request, response) => {
     name,
     passwordHash,
   });
+  try {
+    const savedUser = await user.save();
 
-  const savedUser = await user.save();
-  console.log("username: ", user.username);
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
 });
 
 module.exports = usersRouter;
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -134,6 +134,27 @@ describe("when there is initially one user in db", () => {
     const userNames = usersAtEnd.map((user) => user.username);
     expect(userNames).toContain("root");
   });
+
+  test("creation fails with proper statuscode and message if username already taken", async () => {
+    const usersAtStart = await usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "salainen",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("expected `username` to be unique");
+
+    const usersAtEnd = await usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
 });
 
 afterAll(async () => {
