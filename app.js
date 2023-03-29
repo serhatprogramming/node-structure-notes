6bb2c9778a25414a28a67c74ac8c97d886404efd
serhatprogramming <serhatprogramming@gmail.com>
2023-03-29 10:19:35 -0400

    jwt token auth added to the backend

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 require("express-async-errors");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response, next) => {
   try {
@@ -29,15 +38,18 @@ notesRouter.get("/:id", async (request, response, next) => {
 
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
-
-  const user = await User.findById(body.userId);
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    user: user.id,
-  });
   try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+    if (!decodedToken.id) {
+      response.status(401).json({ error: "invalid token" });
+    }
+    const user = await User.findById(decodedToken.id);
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      user: user.id,
+    });
     const savedNote = await note.save();
     user.notes = user.notes.concat(savedNote._id);
     await user.save();
